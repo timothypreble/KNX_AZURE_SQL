d856f85cb49c4092c2945dbd21ce8917a66185f6
Timothy Preble <tpreble@usxpress.com>
2023-08-24 12:57:28 -0400

    Renamed T -> TH, L->TL

--- getTariffBillToInfo_ByList.sql
+++ getTariffBillToInfo_ByList.sql
@@ -14,6 +14,7 @@ GO
  07/24/2023 - Timothy Preble	- Add parent
  08/07/2023 - Timothy Preble	- Add lastDate, return only the most recent updated record.
  08/24/2023 - Timothy Preble  - Remove commented out code.
+                                Renamed T -> TH, L->TL
 ==============================================================================================================================
  Indexes: 
  DatabaseName.Schema.IndexName
@@ -25,12 +26,11 @@ GO
   VALUES ('01', 885698),('01', 1001183),('01', 1171822),('01', 1210032)
   EXEC dbo.getTariffBillToInfo_ByList @pCompanyAccountList = @TVP -- CompanyAcountList
 ==============================================================================================================================
-*/
+
 ALTER PROCEDURE [dbo].[getTariffBillToInfo_ByList]
 (@pCompanyAccountList dbo.[CompanyCustomerList] READONLY)
 AS
-
-
+*/
 /**Test Object/Data*/
 /*
 DECLARE @pCompanyAccountList dbo.CompanyCustomerList;
@@ -40,31 +40,31 @@ VALUES ( '01', 885698 ) , ( '01', 1001183 ) , ( '01', 1171822 ) , ( '01', 121003
 */
 
 DROP TABLE IF EXISTS #BillTo;
-SELECT H.company_id
-     , H.customer_bill_to_id                                                                         AS customer_bill_to_number
-     , H.customer_bill_to_name
+SELECT TH.company_id
+     , TH.customer_bill_to_id                                                                         AS customer_bill_to_number
+     , TH.customer_bill_to_name
      , CASE
-          WHEN GETDATE() BETWEEN H.contract_effective_date AND H.contract_expiration_date
+          WHEN GETDATE() BETWEEN TH.contract_effective_date AND TH.contract_expiration_date
              THEN 1
           ELSE 0
        END                                                                                           AS is_active
      , COUNT(*)                                                                                      AS active_number_lanes
-     , MAX(COALESCE(L.updated_datetime, L.created_datetime, H.updated_datetime, H.created_datetime)) AS lastDate
+     , MAX(COALESCE(TL.updated_datetime, TL.created_datetime, TH.updated_datetime, TH.created_datetime)) AS lastDate
       INTO #BillTo
-      FROM dbo.tariff_header          AS H
-      LEFT OUTER JOIN dbo.tariff_lane AS L
-        ON            H.tariff_header_Id = L.tariff_header_Id
-                      AND L.isdeleted    = '0'
-                      AND GETDATE() BETWEEN L.lane_effective_date AND L.lane_expiration_date
+      FROM dbo.tariff_header          AS TH
+      LEFT OUTER JOIN dbo.tariff_lane AS TL
+        ON            TH.tariff_header_Id = TL.tariff_header_Id
+                      AND TL.isdeleted    = '0'
+                      AND GETDATE() BETWEEN TL.lane_effective_date AND TL.lane_expiration_date
       JOIN @pCompanyAccountList       AS A
-        ON H.company_id                  = A.Company
-           AND H.customer_bill_to_id     = A.Customer
-     WHERE H.isdeleted = '0'
-     GROUP BY H.company_id
-            , H.customer_bill_to_id
-            , H.customer_bill_to_name
+        ON TH.company_id                  = A.Company
+           AND TH.customer_bill_to_id     = A.Customer
+     WHERE TH.isdeleted = '0'
+     GROUP BY TH.company_id
+            , TH.customer_bill_to_id
+            , TH.customer_bill_to_name
             , CASE
-                 WHEN GETDATE() BETWEEN H.contract_effective_date AND H.contract_expiration_date
+                 WHEN GETDATE() BETWEEN TH.contract_effective_date AND TH.contract_expiration_date
                     THEN 1
                  ELSE 0
               END;
